2fad68bbdee2dbceeebe5c276e88c6edcbb049f6
Tim Mutton <timothymutton@gmail.com>
2012-08-06 14:55:06 +1000

    converting play to agent

--- Assets/_scripts/PlayerMovement.cs
+++ Assets/_scripts/PlayerMovement.cs
@@ -1,24 +1,40 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {	
+public class PlayerMovement : MonoBehaviour 
+{	
 	public float speed = 1.0f;
 	
-	CharacterController controller;
+	Agent agent;
+	AlignSteer align = new AlignSteer();
 	
-	void Start() {
-			controller = gameObject.GetComponent<CharacterController>();
+	//CharacterController controller;
+	
+	void Start() 
+	{
+		agent = gameObject.AddComponent<Agent>();
+        DebugUtil.Assert(agent != null);
+		
+		agent.AddBehaviour("align", align, 0);
+		agent.MaxAngularVelocity = 1.0f;
+		agent.MaxVelocity = speed;
+		
+		align.MaxAngularAcceleration = 0.5f;
+		
+			//controller = gameObject.GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		var mousePosition = Camera.mainCamera.ScreenToWorldPoint(Input.mousePosition);
-		mousePosition.y = 0;
-		transform.LookAt(mousePosition);
+		var vectorised = new Vector2(mousePosition.x, mousePosition.z);
+
+		align.Target.Orientation = MotionUtils.SetOrientationFromVector(vectorised);
+		//transform.LookAt(mousePosition);
 		
-		var movementDirection = transform.TransformDirection(
+		/*var movementDirection = transform.TransformDirection(
 			new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))
-		);
-		controller.Move( movementDirection * speed * Time.deltaTime);
+		;
+		controller.Move( movementDirection * speed * Time.deltaTime);*/
 	}
 }
