423f5db4c4a8b195c8bc6cb92ce4244bbb5ba16b
Dag Einar Monsen <monsen.dag@gmail.com>
2012-08-07 18:45:24 +1000

    Create Camper and Werewolf class.

---
+++ Assets/_scripts/_agentsTypes/Camper.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Camper : Agent {
+
+	void Start () {
+	
+		base.Start ();
+		
+		
+	}
+	
+	void Update () {
+	
+		base.Update();
+	}
+}
---
+++ Assets/_scripts/_agentsTypes/Werewolf.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Werewolf : Agent
+{
+
+	void Start ()
+	{
+		base.Start ();
+		
+	}
+	
+	void Update ()
+	{
+		base.Update ();
+	}
+}
