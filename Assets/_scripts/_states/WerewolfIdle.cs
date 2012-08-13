7c0ba10ca4813467dd76acf70e83f06369d3dcbd
Dag Einar Monsen <monsen.dag@gmail.com>
2012-08-13 17:03:20 +1000

    Implementation of WerewolfIdle state

--- Assets/_scripts/_states/WerewolfIdle.cs
+++ Assets/_scripts/_states/WerewolfIdle.cs
@@ -2,28 +2,31 @@ using UnityEngine;
 
 public class WerewolfIdle : IAgentState
 {
-	public WerewolfIdle ()
+	public WerewolfIdle()
 	{
 		
 	}
 	
-	public void Update (Agent agent, out IAgentState nextState)
+	public void Update(Agent agent, out IAgentState nextState)
 	{
 		nextState = this;
+
+		Agent target = null, attacker = null;
+
 		
 		/// Has target, target not in range -> Charge
 		if (true) {
-			nextState = new WerewolfCharge ();
+			nextState = new WerewolfCharge(target);
+		}
+		
+		/// Has target, target in range -> Attack
+		if (true) {
+			nextState = new WerewolfAttack(target);
 		}
 		
 		/// Attacked -> Evade
 		if (true) {
-			nextState = new WerewolfCharge ();
-		}
-		
-		/// Attacked -> Evade
-		if (true) {
-			nextState = new WerewolfCharge ();
+			nextState = new WerewolfEvade(attacker);
 		}
 	}
 }
