cbc58ed8f878c3a32f951fa7efbd66f3526bbe51
Dag Einar Monsen <me@dag.im>
2012-08-14 22:13:16 +1000

    Add WerewolfHunt stage, to wrap Idle, Charge and Attack.

---
+++ Assets/_scripts/_states/WerewolfHunt.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+/// <summary>
+/// Werewolf hunt.
+/// 
+/// This state is a hierarchial state machine, and 
+/// should contain WerewolfIdle, WerewolfCharge and WerewolfAttack
+/// </summary>
+
+public class WerewolfHunt : AgentStateMachine
+{
+
+	public void initAction()
+	{
+
+	}
+
+	public void exitAction()
+	{
+
+	}
+
+	
+	public new void Update(out AgentStateMachine nextState)
+	{
+			
+		/// camper is attacked -> Evade
+		//if (true) { 
+		nextState = new CamperEvade(); 
+		//} 
+
+
+		nextState = this;
+		// Allow the agent state to update.
+		if (CurrentState != null) {
+			AgentStateMachine nextSubstate;
+			CurrentState.Update(out nextSubstate);
+			CurrentState = nextSubstate;
+		}
+	}	
+}
