09b6301647810756cf9de19c34d3bfe4037eceb3
Dag Einar Monsen <monsen.dag@gmail.com>
2012-08-14 14:48:13 +1000

    Change Wanderer to extend Agent instead of MonoBehaviour.

--- Assets/_scripts/_agentsTypes/Wanderer.cs
+++ Assets/_scripts/_agentsTypes/Wanderer.cs
@@ -5,53 +5,55 @@
 using UnityEngine;
 using System.Collections;
 
-public class Wanderer : MonoBehaviour 
+public class Wanderer : Agent
 {
-    Vector2 target;
-    Agent agent;
+	Vector2 target;
 
 	// Use this for initialization
-	void Start () 
-    {
-        // Add an agent component.
-        agent = gameObject.AddComponent<Agent>();
-        DebugUtil.Assert(agent != null);
-        // And set it up with a wandering state.
-        agent.MaxVelocity = 4.0f;
-        agent.MaxAcceleration = 4.0f;
-        agent.MaxAngularVelocity = 2 * Mathf.PI;
+	public override void Start()
+	{
+		base.Start();
+		// And set it up with a wandering state.
+		MaxVelocity = 4.0f;
+		MaxAcceleration = 4.0f;
+		MaxAngularVelocity = 2 * Mathf.PI;
 
-        WanderSteer wanderSteer = new WanderSteer();
+		WanderSteer wanderSteer = new WanderSteer();
 
-        wanderSteer.MaxAcceleration = 4.0f; 
-        wanderSteer.TargetRadius = 0.025f;
-        wanderSteer.SlowRadius = 0.5f;
-        wanderSteer.MaxOrientationChange = Mathf.PI / 4; // 45 degrees
-        wanderSteer.WanderOffset = 2.9f;
-        wanderSteer.WanderRadius = 0.75f;
+		wanderSteer.MaxAcceleration = 4.0f; 
+		wanderSteer.TargetRadius = 0.025f;
+		wanderSteer.SlowRadius = 0.5f;
+		wanderSteer.MaxOrientationChange = Mathf.PI / 4; // 45 degrees
+		wanderSteer.WanderOffset = 2.9f;
+		wanderSteer.WanderRadius = 0.75f;
 
-        ArriveSteer arriveSteer = new ArriveSteer();
-        SeekSteer seekSteer = new SeekSteer();
+		ArriveSteer arriveSteer = new ArriveSteer();
+		SeekSteer seekSteer = new SeekSteer();
 
-        seekSteer.MaxAcceleration = 2.0f;
+		seekSteer.MaxAcceleration = 2.0f;
 
-        arriveSteer.MaxVelocity = 2.0f;
-        arriveSteer.TargetRadius = 0.2f;
-        arriveSteer.MaxAcceleration = 2.0f;
-        arriveSteer.SlowRadius = 1.5f;
-        arriveSteer.TimeToTarget = 1.0f;
-        target.x = Random.Range(-5.0f, 5.0f);
-        target.y = Random.Range(-5.0f, 5.0f);
+		arriveSteer.MaxVelocity = 2.0f;
+		arriveSteer.TargetRadius = 0.2f;
+		arriveSteer.MaxAcceleration = 2.0f;
+		arriveSteer.SlowRadius = 1.5f;
+		arriveSteer.TimeToTarget = 1.0f;
+		target.x = Random.Range(-5.0f, 5.0f);
+		target.y = Random.Range(-5.0f, 5.0f);
 
-        arriveSteer.Target.Position = target;
-        seekSteer.Target.Position = target;
+		arriveSteer.Target.Position = target;
+		seekSteer.Target.Position = target;
 
-        //agent.AddBehaviour("arrive", arriveSteer, 0);
+		//AddBehaviour("arrive", arriveSteer, 0);
 
-        //agent.AddBehaviour("LWYG",new LWYGSteer(),0);
-        agent.AddBehaviour("wander", wanderSteer, 0);
-        //agent.AddBehaviour("seek", seekSteer, 0);
+		//AddBehaviour("LWYG",new LWYGSteer(),0);
+		AddBehaviour("wander", wanderSteer, 0);
+		//AddBehaviour("seek", seekSteer, 0);
         
+	}
+
+	public override void Update()
+	{
+		base.Update();
 	}
 
 }
