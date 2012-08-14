c427a32228a591065c8c072cf1be928e6050915f
Dag Einar Monsen <me@dag.im>
2012-08-14 22:13:49 +1000

    Add static class for simple access to configuration variables.

---
+++ Assets/_scripts/Config.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class Config
+{
+	public const float WerewolfAttackRange = 5f;
+	public const float WerewolfChargeRange = 5f;
+	public const float CamperFlockRadius = 5f;
+
+}
+
