eca41c72e43607e124262c92fb57a4f09461f341
Tim <Tim@Tim-PC>
2012-09-28 10:35:07 +1000

    record agent to csv

--- Assets/_scripts/_decisionTree/_decisions/AgentHealth.cs
+++ Assets/_scripts/_decisionTree/_decisions/AgentHealth.cs
@@ -3,15 +3,7 @@ using System.Collections;
 
 public class AgentHealth : IDecision
 {
-	Agent _agent;
-	uint _threshold;
-	
-	AgentHealth(Agent agent, uint threshold){
-		_agent = agent;
-		_threshold = threshold;
-	}
-	
 	public bool Decide(){
-		return _agent.Health < _threshold;
+        return true;
 	}
 }
