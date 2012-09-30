0d79b054b0cdf31ae01e34902923effc44669c5d
Darcy <Darcy@Scudnik-Portabl>
2012-09-30 14:08:10 +1000

    Added visibility check to DistanceToTarget.

--- Assets/_scripts/_decisionTree/_decisions/DistanceToTarget.cs
+++ Assets/_scripts/_decisionTree/_decisions/DistanceToTarget.cs
@@ -12,6 +12,18 @@ public class DistanceToTarget : IValue
         
         if (target == null) { return OutputNumber - 1; }
 
+        // Test the visability of the target.
+        Vector3 direction = (target.transform.position - agent.transform.position).normalized;
+        var hits = Physics.RaycastAll(agent.transform.position, direction, Config.DefaultWerewolfVisionRange);
+        foreach (var hit in hits)
+        {
+            if (hit.collider.gameObject != agent.gameObject && hit.collider.gameObject != target.gameObject)
+            {
+                return (OutputNumber - 1);
+            }
+        }
+        
+
 		float dist = Vector2.Distance(agent.KinematicInfo.Position, target.KinematicInfo.Position);
         dist = Mathf.Clamp(dist, 0.0f, MaxDistance);
         dist = (OutputNumber - 1) * dist / MaxDistance;
